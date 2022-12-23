c715da788be5556a40e67d19e677b486a44b3272
Riasat Mahbub <Riasat.Mahbub@brainstation-23.com>
2022-12-23 13:25:53 +0600

    added a new global prisma module to make data access easier

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from "@nestjs/common";
+import { User, Bookmark } from "@prisma/client";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable({})
-
 export class AuthService{
+    constructor(private prisma: PrismaService){
+
+    }
     login(){
         return {msg: "Logged in"};
     }
