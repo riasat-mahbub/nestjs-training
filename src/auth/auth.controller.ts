1ffcff2e88fce3c57ff9e3240c53be6ae09db612
Riasat Mahbub <Riasat.Mahbub@brainstation-23.com>
2022-12-23 11:44:39 +0600

    added a new login and register post routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
 
 @Controller()
 
 export class AuthController{
+    constructor(private authService: AuthService){
+    }
 
+    @Post('login')
+    login(){
+        return " I am logged in"
+    }
+
+    @Post('register')
+    register(){
+        return " I have been registered"
+    }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,5 +3,7 @@ import { Injectable } from "@nestjs/common";
 @Injectable({})
 
 export class AuthService{
+    login(){}
 
+    register(){}
 }
