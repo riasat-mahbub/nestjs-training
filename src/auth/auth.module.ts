a25e40a028dd7610a04f12dac2d741ca9e6a24e4
Riasat Mahbub <Riasat.Mahbub@brainstation-23.com>
2022-12-23 11:22:12 +0600

    created controllers and services for auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,11 @@
 import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
 
 @Module({
-    imports: [AuthModule]
+    imports: [AuthModule],
+    controllers: [AuthController],
+    providers: [AuthService]
 })
 
 export class AuthModule{
