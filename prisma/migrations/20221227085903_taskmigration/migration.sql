4e57a8a969bcbb2aff91cd06b4d0a86dccc3f4c0
Riasat Mahbub <Riasat.Mahbub@brainstation-23.com>
2022-12-27 16:46:26 +0600

    Added all tasks as prisma objects and migrations

---
+++ prisma/migrations/20221227085903_taskmigration/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Tasks" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "text" TEXT NOT NULL,
+    "day" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "reminder" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Tasks_pkey" PRIMARY KEY ("id")
+);
