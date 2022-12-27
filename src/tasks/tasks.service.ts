ae63f9a7a9de7e5c7f32d7847d6c02891a5f61c7
Riasat Mahbub <Riasat.Mahbub@brainstation-23.com>
2022-12-27 16:49:32 +0600

    created a service for tasks objects with CRUD

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { TasksDto } from "./dto";
+
+@Injectable({})
+export class TasksService{
+    constructor(private prisma: PrismaService){}
+
+    async create(dto: TasksDto){
+        const task = await this.prisma.tasks.create({
+            data: {
+                text: dto.text,
+                reminder: dto.reminder
+            }
+        })
+
+        return task
+    }
+
+    async read(id: number){
+        const allTasks = await this.prisma.tasks.findMany()
+        return allTasks;
+    }
+
+    async readSingle(id: number){
+        const singleTask = await this.prisma.tasks.findUnique({
+            where:{
+                id: id,
+            }
+        })
+
+        return singleTask
+    }
+
+    async update(id: number, dto: TasksDto){
+        const task = this.prisma.tasks.update({
+            where:{
+                id: id
+            },
+            data:{
+                text: dto.text,
+                reminder: dto.reminder
+            }
+        })
+
+        return task;
+    }
+
+    async delete(id: number){
+        const task = this.prisma.tasks.delete({
+            where:{
+                id: id
+            }
+        })
+
+        return task;
+    }
+
+
+}
